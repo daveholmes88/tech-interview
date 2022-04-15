c2db1bc150376a2298674f985bf29df6544943c3
Dave Holmes <daveholmes88@gmail.com>
2022-04-15 15:47:18 -0500

    finished

--- README.md
+++ README.md
@@ -1,70 +1,13 @@
-# Getting Started with Create React App
+Using Javascript & React, please build a kanban board! This project should meet the following requirements:
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1. The kanban board should have 3 swimlanes.
 
-## Available Scripts
+2. There is functionality to add & delete a ticket.
 
-In the project directory, you can run:
+3. Each ticket should have the ability to have a name and description.
 
-### `npm start`
+4. Allow each ticket to move between swimlanes. This can be accomplished with buttons on each ticket.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+5. Add basic styling using a CSS library of your choice.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+6. There is no requirement to persist these tickets in a database.
--- src/App.js
+++ src/App.js
@@ -1,24 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import uuid from 'react-uuid';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+`;
+
+const Column = styled.div`
+  width: 300px;
+  margin: 20px;
+  justify-content: center;
+`
+
+const Card = styled.div`
+  width: 200px;
+  outline: 5px solid black;
+`
 
 function App() {
+  const [description, setDescription] = useState('')
+  const [title, setTitle] = useState('')
+  const [list1, setList1] = useState([])
+  const [list2, setList2] = useState([])
+  const [list3, setList3] = useState([])
+
+  const newItem = (e) => {
+    e.preventDefault();
+    const item = {
+      id: uuid(),
+      description,
+      title
+    }
+    setList1([...list1, item]);
+    setDescription('');
+    setTitle('')
+  }
+
+  const advanceToList2 = (item) => {
+    const newList = list1.filter(list => list.id !== item.id)
+    setList1(newList)
+    setList2([...list2, item])
+  }
+
+  const createItemList1 = (item) => {
+    return (
+      <Card key={item.id}>
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+        <br></br>
+        <button onClick={() => advanceToList2(item)}>{'>'}</button>
+        <br></br>
+        <button onClick={() => deleteItem(item, list1, setList1)}>Delete</button>
+      </Card>
+    )
+  }
+
+  const advanceToList3 = (item) => {
+    const newList = list2.filter(list => list.id !== item.id)
+    setList2(newList)
+    setList3([...list3, item])
+  }
+
+  const downToList1 = (item) => {
+    const newList = list2.filter(list => list.id !== item.id)
+    setList2(newList)
+    setList1([...list1, item])
+  }
+
+  const downToList2 = (item) => {
+    const newList = list3.filter(list => list.id !== item.id)
+    setList3(newList)
+    setList2([...list2, item])
+  }
+
+  const createItemList2 = (item) => {
+    return (
+      <Card key={item.id}>
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+        <br></br>
+        <button onClick={() => downToList1(item)}>{'<'}</button>
+        <button onClick={() => advanceToList3(item)}>{'>'}</button>
+        <br></br>
+        <button onClick={() => deleteItem(item, list2, setList2)}>Delete</button>
+      </Card>
+    )
+  }
+
+  const createItemList3 = item => {
+    return (
+      <Card key={item.id}>
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+        <br></br>
+        <button onClick={() => downToList2(item)}>{'<'}</button>
+        <br></br>
+        <button onClick={() => deleteItem(item, list3, setList3)}>Delete</button>
+      </Card>
+    )
+  }
+
+  const deleteItem = (item, list, method) => {
+    const newList = list.filter(thing => thing.id !== item.id)
+    method(newList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>KANBAN BOARD</h1>
+      <Column>
+        <form>
+        <label for="titlen">Title:</label>
+        <br></br>
+        <input onChange={(e) => setTitle(e.target.value)} type="text" id="title" name="title" value={title} />
+        <br></br>
+        <label for="description">Description:</label>
+        <br></br>
+        <input onChange={(e) => setDescription(e.target.value)} type="text" id="description" name="description" value={description} />
+        <br></br>
+        <input type="submit" value="Submit" onClick={(e) => newItem(e)}/>
+        </form>
+      </Column>
+      <Column>
+        <h2>List 1</h2>
+        {list1.map(item => createItemList1(item))}
+      </Column>
+      <Column>
+        <h2>List 2</h2>
+        {list2.map(item => createItemList2(item))}
+      </Column>
+      <Column>
+      <h2>List 3</h2>
+        {list3.map(item => createItemList3(item))}
+      </Column>
+    </Container>
   );
 }
 
